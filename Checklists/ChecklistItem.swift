f6ace3ff50da385f7703e4a7f94e6199ec400934
Bektur Mamytov <bekturmamytov@gmail.com>
2023-01-20 06:27:25 +0900

    Initial Commit

---
+++ Checklists/ChecklistItem.swift
@@ -0,0 +1,13 @@
+//
+//  ChecklistItem.swift
+//  Checklists
+//
+//  Created by Bektur Mamytov on 19/1/23.
+//
+
+import Foundation
+
+class ChecklistItem {
+    var text = ""
+    var checked = false
+}
---
+++ Checklists/ChecklistViewController.swift
@@ -0,0 +1,107 @@
+//
+//  ViewController.swift
+//  Checklists
+//
+//  Created by Bektur Mamytov on 15/1/23.
+//
+
+import UIKit
+
+class ChecklistViewController: UITableViewController {
+    var items = [ChecklistItem]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let item1 = ChecklistItem()
+        item1.text = "Walk the dog"
+        items.append(item1)
+        
+        let item2 = ChecklistItem()
+        item2.text = "Brush my teeth"
+        item2.checked = true
+        items.append(item2)
+        
+        let item3 = ChecklistItem()
+        item3.text = "Learn iOS Development"
+        item3.checked = true
+        items.append(item3)
+        
+        let item4 = ChecklistItem()
+        item4.text = "Soccer practice"
+        items.append(item4)
+        
+        let item5 = ChecklistItem()
+        item5.text = "Eat ice cream"
+        item5.checked = true
+        items.append(item5)
+    }
+    
+    @IBAction func addItem() {
+        //I need to know what the index of the new row in my array.
+        let newRowIndex = items.count
+        
+        // This code creates new item from data model and adds to items array.
+        let item = ChecklistItem()
+        item.text = "I am new row"
+        item.checked = true
+        items.append(item)
+        
+        //I have to tell to tableview about this new row so it can add a new cell for that row.
+        let indexPath = IndexPath(row: newRowIndex, section: 0)
+        let indexPaths = [indexPath]
+        //to insert a new row to the table view 
+        tableView.insertRows(at: indexPaths, with: .automatic)
+    }
+    
+    func configureChackmark(for cell: UITableViewCell, with item: ChecklistItem) {
+        if item.checked {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+        }
+    }
+    
+    func configureText(for cell: UITableViewCell, with item: ChecklistItem) {
+        let label = cell.viewWithTag(1000) as! UILabel
+        label.text = item.text
+    }
+    
+}
+
+//MARK: - Table View Data Source
+extension ChecklistViewController {
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return items.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
+        
+        let item = items[indexPath.row]
+        
+        configureText(for: cell, with: item)
+        // cell yaratilmadan once cell configure edilmesini saglayan method
+        configureChackmark(for: cell, with: item)
+        return cell
+    }
+}
+
+//MARK: - Table View Delegate
+
+extension ChecklistViewController {
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let cell = tableView.cellForRow(at: indexPath) {
+            let item = items[indexPath.row]
+            item.checked.toggle()
+            
+            configureChackmark(for: cell, with: item)
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
